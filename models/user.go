d03864cc743439709d313e1411230a0aa1d29878
imrkaofficial <rajat.g@siprtc.io>
2024-04-14 02:57:52 +0530

    update ui of signin, signup and forgot password

--- models/user.go
+++ models/user.go
@@ -4,7 +4,9 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Username string `gorm:"unique;not null"`
-	Password string `gorm:"not null"`
-	Email    string `gorm:"unique;not null"`
+	Username	string	`gorm:"unique;not null"`
+	Email    	string	`gorm:"unique;not null"`
+	FirstName	string	`gorm:"type:varchar(255)"`
+	LastName	string	`gorm:"type:varchar(255)"`
+	Password 	string	`gorm:"not null"`
 }
