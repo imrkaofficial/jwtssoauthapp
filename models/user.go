4eb7fcaca7f200f0dd0b6053a62ba1dcd89d912a
imrkaofficial <imrkgoyal@gmail.com>
2024-04-14 21:16:53 +0530

    update user model

--- models/user.go
+++ models/user.go
@@ -4,9 +4,16 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	Username	string	`gorm:"unique;not null"`
-	Email    	string	`gorm:"unique;not null"`
-	FirstName	string	`gorm:"type:varchar(255)"`
-	LastName	string	`gorm:"type:varchar(255)"`
-	Password 	string	`gorm:"not null"`
+	Username	string	`json:"username" gorm:"unique;not null"`
+	Email    	string	`json:"email" gorm:"unique;not null"`
+	FirstName	string	`json:"first_name" gorm:"type:varchar(255)"`
+	LastName	string	`json:"last_name" gorm:"type:varchar(255)"`
+	Password 	string	`json:"password" gorm:"not null"`
+	PerPhone	string	`json:"per_phone" gorm:"type:varchar(255)"`
+	AltPhone	string	`json:"alt_phone" gorm:"type:varchar(255)"`
+	Balance		string	`json:"balance" gorm:"type:decimal(12,6)"`
+	Address		string	`json:"address" gorm:"type:varchar(255)"`
+	City		string	`json:"city" gorm:"type:varchar(255)"`
+	Country		string	`json:"country" gorm:"type:varchar(255)"`
+	Pincode		string	`json:"pincode" gorm:"type:varchar(255)"`
 }
